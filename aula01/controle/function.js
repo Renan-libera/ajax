69f4b024eeb5acc5b9ab4a9b528ec0b6a184fccc
Renan-libera <renanhenri12@hotmail.com>
2022-03-08 10:40:26 -0300

    finalizaçao

--- aula01/controle/function.js
+++ aula01/controle/function.js
@@ -1,23 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    // Monitorar o clique em cima do botão btn-send
-    $('.btn-send').click(function(e){
+    $('.btn-send').click(function(e) {
         e.preventDefault()
 
-    //Coletar as informacoes digitadas no formulario
-    let dados = $('#form').serialize()
+        let dados = $('#form').serialize()
 
-    
-    $.ajax({
-        type: 'POST',
-        datatype: 'JSON',
-        assync: true,
-        data: dados,
-        url: '../modelo/retorno.php',
-        success: function(){
-            
-        }
-    })
+        $('#retorno').empty()
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'JSON',
+            assync: true,
+            data: dados,
+            url: '../modelo/retorno.php',
+            success: function(dados) {
+                console.log(dados)
+                $('#retorno').append(`
+                        <div class="col-12 col-sm-8 col-md-6">
+                            <div class="alert-primary">
+                                <h1 class="text-white text-center">
+                                  ${dados.mensagem}
+                                </h1>
+                                <img src="../../img/${dados.tipo}" class="img-fluid">
+                            </div>
+                        </div>
+                `)
+
+            }
+        })
     })
 
 })
